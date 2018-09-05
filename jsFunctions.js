ac6b77c260806436463d8ffd325d3468be95b7cd
Infernocreator33 <zs0877310@otc.edu>
2018-09-05 10:18:27 -0500

    jsFunctions Assignments

---
+++ jsFunctions.js
@@ -0,0 +1,30 @@
+function square1(x)
+{
+    return Math.pow(x,2);
+}
+    for(let i = 0; i <= 10; i++)
+    {
+        console.log(square1(i));
+    }
+
+
+const square2 = x => Math.pow(x,2);
+
+for(let i = 0; i <= 10; i++)
+{
+    console.log(square2(i));
+}
+
+const min = (x, y) => 
+{
+    if(x < y)
+        return x;
+    if(x > y)
+        return y;
+    if(x = y)
+        return "The first and second number are equal";    
+        
+}
+console.log(min(4,5));
+console.log(min(5,7));
+console.log(min(25, 30));

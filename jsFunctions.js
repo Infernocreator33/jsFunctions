ebca24682a0782bc1afb4f4c84136f285140c866
Infernocreator33 <35383954+Infernocreator33@users.noreply.github.com>
2018-09-05 10:20:34 -0500

    Update jsFunctions.js

--- jsFunctions.js
+++ jsFunctions.js
@@ -6,8 +6,6 @@ function square1(x)
     {
         console.log(square1(i));
     }
-
-
 const square2 = x => Math.pow(x,2);
 
 for(let i = 0; i <= 10; i++)
@@ -22,9 +20,9 @@ const min = (x, y) =>
     if(x > y)
         return y;
     if(x = y)
-        return "The first and second number are equal";    
-        
+        return "The first and second number are equal";          
 }
 console.log(min(4,5));
 console.log(min(5,7));
 console.log(min(25, 30));
+console.log(min(20, 20));
